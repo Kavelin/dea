f7094d7cebd1bacc07e5ac1ded4d49ae7ef690e6
Kavelin <34014055+Kavelin@users.noreply.github.com>
2023-11-24 18:00:26 +0000

    fully player works!

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,9 @@
 
             </div>
             <div id="mid">
-                <button title="play">⏯</button>
-                <button title="stop">🟦</button>
+                <button title="play" @click="playBtn">⏯</button>
+                <button title="stop" @click="stopBtn">🟦</button>
+                <button title="warpTo" @click="warpBtn">⚓</button>
                 <button title="record">🔴</button>
             </div>
             <div id="right">
@@ -27,8 +28,10 @@
                         <div class="bef-space">
                             Measures
                         </div>
-                        <div id="position" :style="{height:positionDomHeight + 'px', display:'block'}"></div>
-                        <div class="measure-c" :style="{ width: (150 * zoom.x + 'px ') }" :key="i" v-for="i in 1000">
+                        <div id="position"
+                            :style="{ height: positionDomHeight + 'px', display: 'block', left: (position * 150 * zoom.x) + 'px' }">
+                        </div>
+                        <div class="measure-c" :style="{ width: (150 * zoom.x + 'px ') }" :key="i" v-for="i in 50">
                             {{ i }}
                         </div>
                     </div>
@@ -61,28 +64,70 @@ interface Part {
 
 let zoom = ref({ x: 1 });
 let position = ref(0); //in measures
+let positionDomHeight = ref(0);
+
 let tempo = 120;
+let timeSig = {
+    top: 4, bottom: 4
+}
 let tempoInput = (e: Event) => {
     e.preventDefault();
     let val = Number((<HTMLDivElement>e.target!).innerText);
     if (!isNaN(val)) tempo = (val > 999) ? 999 : (val < 10) ? 10 : val;
     (<HTMLDivElement>e.target!).innerText = tempo.toFixed(0).toString();
 }
+let playBtn = () => {
+    lastTime = Date.now(); 
+    stopped = !stopped;
+    playing();
+}
 
-let positionDomHeight = ref(0);
+let stopBtn = () => { 
+    position.value = 0;
+    stopped = true;
+}
+let warpBtn = () => {
+    document.querySelector('.sheet')!.scrollLeft! = position.value * zoom.value.x * 150;
+
+}
+
+
 
 
 let parts = ref(<Part[]>[{ name: "piano", type: "midi", content: {} }]);
-let keys = ref(<{ [key: string]: any }>{});
+let keys = <{ [key: string]: any }>{};
 onMounted(() => {
     document.addEventListener("keydown", e => {
-        keys.value[e.key] = true;
-        if (e.key == "=") zoom.value.x -= 0.1;
-        if (e.key == "-") zoom.value.x += 0.1;
-        if (zoom.value.x >= 3) zoom.value.x = 3;
+        keys[e.key] = true;
+        if (e.key == "-") zoom.value.x -= 0.1;
+        if (e.key == "=") zoom.value.x += 0.1;
+        if (zoom.value.x >= 10) zoom.value.x = 10;
         if (zoom.value.x <= 0.1) zoom.value.x = 0.1;
+        if (e.key == " ") {
+            e.preventDefault();
+            playBtn();
+        }
+        if (e.key == "Home") {
+            position.value = 0;
+        }
     });
-    document.addEventListener("keyup", e => keys.value[e.key] = false);
-    positionDomHeight.value = document.querySelector('.sheet')?.clientHeight!;
-})
+    document.addEventListener("keyup", e => keys[e.key] = false);
+
+    positionDomHeight.value = document.querySelector('.sheet')!.clientHeight!;
+    lastTime = Date.now();
+    playing();
+
+});
+
+let lastTime = 0;
+let stopped = true;
+let playing = () => {
+    if (!stopped) {
+        //console.log(keys);
+        let timeEla = Date.now() - lastTime;
+        position.value += timeEla / ((240000 / (timeSig.bottom) / tempo) * timeSig.top);
+        lastTime = Date.now();
+        requestAnimationFrame(playing);
+    }
+}
 </script>
