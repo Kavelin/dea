3cf964a868a0780ed6814fbd191e6a1b2dc64faa
Kavelin <34014055+Kavelin@users.noreply.github.com>
2023-11-25 03:47:17 +0000

    start midi

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
         <div id="top-bar">
             <div id="left">
                 <div contenteditable id="tempo" title="tempo" size="1" type="number" @keydown.enter="tempoInput"
-                    @focusout="tempoInput"> 120 </div>
-
+                    @focusout="tempoInput">120</div>
+                <div>{{ timeSig.top }} / {{ timeSig.bottom }}</div>
             </div>
             <div id="mid">
                 <button title="play" @click="playBtn">⏯</button>
@@ -41,6 +41,15 @@
                             {{ part.name }}
                         </div>
                         <div class="edit" :class="{ 'midi': part.type == 'midi', 'audio': part.type == 'audio' }">
+                            <div class="clip" v-for="clip, i in part.content" :key="i"
+                                :style="{ width: (150 * zoom.x * clip.duration + 'px '), left: (clip.location * 150 * zoom.x) + 'px' }">
+                                <div class="clip-grap"></div>
+                                <div class="midi-content" v-if="part.type == 'midi' && 'notes' in clip">
+                                    <div v-for="note, j in clip.notes" :key="j" class="note"
+                                        :style="{ width: note.duration * 150 * zoom.x + 'px', left: note.location * 150 * zoom.x + 'px' }">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -59,12 +68,56 @@
 interface Part {
     type: string;
     name: string;
-    content: object;
+    content: MidiClip[] | AudioClip[];
+    noteRange?: {min:number, max:number};
+}
+
+interface MidiClip {
+    location: number;
+    duration: number;
+    notes: Array<Note>;
+}
+interface AudioClip {
+    location: number;
+    duration: number;
+    src: string;
+}
+
+interface Note {
+    location: number;
+    duration: number;
+    pitch: number; // 0 - 127
 }
 
 let zoom = ref({ x: 1 });
 let position = ref(0); //in measures
 let positionDomHeight = ref(0);
+let lastTime = 0;
+let stopped = true;
+let playing = () => {
+    if (!stopped) {
+        let timeEla = Date.now() - lastTime;
+        position.value += timeEla / ((240000 / (timeSig.bottom) / tempo) * timeSig.top);
+        //60000 miliseconds in a minute * 4
+        lastTime = Date.now();
+        requestAnimationFrame(playing);
+    }
+}
+
+let playBtn = () => {
+    lastTime = Date.now();
+    stopped = !stopped;
+    playing();
+}
+
+let stopBtn = () => {
+    position.value = 0;
+    stopped = true;
+}
+let warpBtn = () => {
+    document.querySelector('.sheet')!.scrollLeft = position.value * zoom.value.x * 150;
+}
+
 
 let tempo = 120;
 let timeSig = {
@@ -76,25 +129,32 @@ let tempoInput = (e: Event) => {
     if (!isNaN(val)) tempo = (val > 999) ? 999 : (val < 10) ? 10 : val;
     (<HTMLDivElement>e.target!).innerText = tempo.toFixed(0).toString();
 }
-let playBtn = () => {
-    lastTime = Date.now(); 
-    stopped = !stopped;
-    playing();
-}
 
-let stopBtn = () => { 
-    position.value = 0;
-    stopped = true;
-}
-let warpBtn = () => {
-    document.querySelector('.sheet')!.scrollLeft! = position.value * zoom.value.x * 150;
-
-}
-
-
-
-
-let parts = ref(<Part[]>[{ name: "piano", type: "midi", content: {} }]);
+let parts = ref(<Part[]>[{
+    name: "piano", type: "midi",
+    content: [<MidiClip>{
+        location: 1, duration: 2, notes: [
+            { pitch: 60, location: 0, duration: 0.2 },
+            { pitch: 62, location: 0.2, duration: 0.2 },
+            { pitch: 63, location: 0.4, duration: 0.2 },
+            { pitch: 65, location: 0.6, duration: 0.2 },
+            { pitch: 63, location: 0.8, duration: 0.4 },
+            { pitch: 58, location: 1.2, duration: 0.2 },
+            { pitch: 60, location: 1.4, duration: 0.4 },
+        ]
+    }], 
+}]);
+parts.value.forEach(p => {
+    if (p.type == "midi") {
+        let pitches = p.content.flatMap(c => c.notes.map(n => n.pitch));
+        let min = pitches[0], max = pitches[0];
+        for (let i = 1; i < pitches.length; ++i) {
+          if (pitches[i] > max) max = pitches[i];
+          if (pitches[i] < min) min = pitches[i];
+        }
+        p.noteRange = {min, max};
+    }
+});
 let keys = <{ [key: string]: any }>{};
 onMounted(() => {
     document.addEventListener("keydown", e => {
@@ -118,16 +178,4 @@ onMounted(() => {
     playing();
 
 });
-
-let lastTime = 0;
-let stopped = true;
-let playing = () => {
-    if (!stopped) {
-        //console.log(keys);
-        let timeEla = Date.now() - lastTime;
-        position.value += timeEla / ((240000 / (timeSig.bottom) / tempo) * timeSig.top);
-        lastTime = Date.now();
-        requestAnimationFrame(playing);
-    }
-}
 </script>
