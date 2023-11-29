bd75bb48e1699212dce4737a17115876e11cf982
Kavelin <34014055+Kavelin@users.noreply.github.com>
2023-11-29 23:47:49 +0000

    helpful comments, grid stuff

--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,17 @@
   <div id="container">
     <div id="top-bar">
       <div id="left">
-        <div
-          contenteditable
-          id="tempo"
-          title="tempo"
-          size="1"
-          type="number"
-          @keydown.enter="tempoInput"
-          @focusout="tempoInput"
-        >
-          120
+        <div contenteditable id="tempo" title="tempo" size="1" type="number" @keydown.enter.prevent="inputs"
+          @focusout="inputs">120</div>
+        <div>
+          <div title="time signature top" size="1" contenteditable @keydown.enter.prevent="inputs" @focusout="inputs">{{
+            timeSig.top }}</div> / <div title="time signature bottom" @keydown.enter.prevent="inputs" @focusout="inputs"
+            contenteditable>{{
+              timeSig.bottom }}</div>
         </div>
-        <div>{{ timeSig.top }} / {{ timeSig.bottom }}</div>
       </div>
       <div id="mid">
+        <button title="play" @click="position = 0">⏪</button>
         <button title="play" @click="playBtn">⏯</button>
         <button title="stop" @click="stopBtn">🟦</button>
         <button title="warp to play line" @click="warpBtn">⚓</button>
@@ -25,96 +22,63 @@
     </div>
     <div id="not-top-bar">
       <div id="modify">
-        <div
-          class="grid"
-          :style="{
-            backgroundPosition: `top ${pageModify.pan.y}px left ${pageModify.pan.x}px`,
-          }"
-        >
-          <div
-            class="node-wrap"
-            v-if="pageModify.cur"
-            :style="{
-              transform: `translate(${pageModify.pan.x}px, ${pageModify.pan.x})`,
-            }"
-          >
-            <div
-              class="node"
-              v-for="(node, i) in pageModify.cur.nodes"
-              :key="i"
-            ></div>
+        <div class="grid" :style="{
+          backgroundPosition: `top ${pageModify.pan.y}px left ${pageModify.pan.x}px`,
+        }" @contextmenu.prevent @mousedown="pageModify.pan.moving = true;" @mousemove="($event) => {
+          if (pageModify.pan.moving) {
+            pageModify.pan.y--;
+          }
+        }"
+          @mouseup="pageModify.pan.moving = false;">
+          <div class="node-wrap" v-if="pageModify.cur" :style="{
+            transform: `translate(${pageModify.pan.x}px, ${pageModify.pan.y})`,
+          }">
+            <div class="node" v-for="(node, i) in pageModify.cur.nodes"
+              :style="{ top: node.y + 'px', left: node.x + 'px' }" :key="i">
+
+              <div class="node-title">{{ node.name }}</div>
+
+              <prop left right>
+                <div> in/out </div>
+              </prop>
+            </div>
           </div>
         </div>
       </div>
       <div id="editor">
-        <div
-          class="sheet"
-          @click="pageModify.cur = null"
-          :style="{ gridAutoRows: '24px ' + '150px '.repeat(parts.length) }"
-        >
+        <div class="sheet" @mouseup="pageModify.cur = null"
+          :style="{ gridAutoRows: '24px ' + '150px '.repeat(parts.length) }">
           <div class="measures">
             <div class="bef-space">Measures</div>
-            <div
-              id="position"
-              :style="{
-                height: positionDomHeight + 'px',
-                display: 'block',
-                left: position * 150 * zoom.x + 'px',
-              }"
-            ></div>
-            <div
-              class="measure-c"
-              :style="{ width: 150 * zoom.x + 'px ' }"
-              :key="i"
-              v-for="i in 50"
-            >
+            <div id="position" :style="{
+              height: positionDomHeight + 'px',
+              display: 'block',
+              left: position * 150 * zoom.x + 'px',
+            }"></div>
+            <div class="measure-c" :style="{ width: 150 * zoom.x + 'px ' }" :key="i" v-for="i in 50">
               {{ i }}
             </div>
           </div>
-          <div
-            class="part"
-            :class="{ selected: pageModify.cur == part }"
-            v-for="(part, i) in parts"
-            :key="i"
-            :style="{}"
-            @click.stop="pageModify.cur = part"
-          >
-            <div
-              class="part-name"
-              :class="{ selected: pageModify.cur == part }"
-            >
+          <div class="part" :class="{ selected: pageModify.cur == part }" v-for="(part, i) in parts" :key="i"
+            @mouseup.stop="pageModify.cur = part">
+            <div class="part-name" :class="{ selected: pageModify.cur == part }">
               {{ part.name }}
             </div>
-            <div
-              class="edit"
-              :class="{
-                midi: part.type == 'midi',
-                audio: part.type == 'audio',
-              }"
-            >
-              <div
-                class="clip"
-                v-for="(clip, i) in part.content"
-                :key="i"
-                :style="{
-                  width: 150 * zoom.x * clip.duration + 'px ',
-                  left: clip.location * 150 * zoom.x + 'px',
-                }"
-              >
+            <div class="edit" :class="{
+              midi: part.type == 'midi',
+              audio: part.type == 'audio',
+            }">
+              <div class="clip" v-for="(clip, i) in part.content" :key="i" :style="{
+                width: 150 * zoom.x * clip.duration + 'px ',
+                left: clip.location * 150 * zoom.x + 'px',
+              }">
                 <div class="clip-grap"></div>
-                <div
-                  class="midi-content"
-                  v-if="part.type == 'midi' && 'notes' in clip"
-                >
-                  <div
-                    v-for="(note, j) in clip.notes"
-                    :key="j"
-                    class="note"
-                    :style="{
-                      width: note.duration * 150 * zoom.x + 'px',
-                      left: note.location * 150 * zoom.x + 'px',
-                    }"
-                  ></div>
+                <div class="midi-content" v-if="part.type == 'midi' && 'notes' in clip">
+                  <div v-for="(note, j) in clip.notes" :key="j" class="note" :style="{
+                    width: note.duration * 150 * zoom.x + 'px',
+                    left: note.location * 150 * zoom.x + 'px',
+                    top: ((note.pitch - part.noteRange.max) * 107.5) / (part.noteRange.min - part.noteRange.max) + 10 + 'px'
+                  }"></div>
                 </div>
               </div>
             </div>
@@ -125,9 +89,11 @@
   </div>
 </template>
 <script setup lang="ts">
+
 interface Node {
   x: number;
   y: number;
+  name: string;
   auNode?: AudioNode;
   output?: Node;
   input?: Node;
@@ -138,45 +104,50 @@ interface Part {
   name: string;
   content: MidiClip[] | AudioClip[];
   nodes: Node[];
-  noteRange?: { min: number; max: number };
+  noteRange: { min: number; max: number };
 }
 
 interface MidiClip {
-  location: number;
-  duration: number;
-  notes: Array<Note>;
+  location: number; //in measures
+  duration: number; //in measures
+  notes: Array<Note>; //would a map be better
 }
-interface AudioClip {
-  location: number;
-  duration: number;
+
+interface AudioClip { //still not implemented...
+  location: number; //in measures
+  duration: number; //in measures?
   src: string;
 }
 
 interface Note {
-  location: number;
-  duration: number;
+  location: number; // in measures relative to beginning of clip
+  duration: number;  //in measures
   pitch: number; // 0 - 127
 }
 
 interface Modify {
-  pan: { x: number; y: number };
-  cur: Part | null;
-  master: Node[];
+  pan: {
+    x: number; y: number,
+    moving: boolean,
+  };  //moves the background and nodes
+  cur: Part | null; // if cur is null (no part is selected), 
+  master: Node[]; //   master is shown to send all parts to output
 }
 
 let zoom = ref({ x: 1 });
-let position = ref(0); //in measures
-let positionDomHeight = ref(0);
+let position = ref(0); // playhead position in measures
+let positionDomHeight = ref(0);  //  calculate how tall the playhead is
 let lastTime = 0;
 let stopped = true;
 let playing = () => {
   if (!stopped) {
-    let timeEla = Date.now() - lastTime;
-    position.value +=
+    let timeEla = Date.now() - lastTime;  //time elapsed since last update
+    position.value +=   //positio
       timeEla / ((240000 / timeSig.bottom / tempo) * timeSig.top);
-    //60000 miliseconds in a minute * 4
+    //60000 miliseconds in a minute * 4 / bottom
+    //
     lastTime = Date.now();
-    requestAnimationFrame(playing);
+    requestAnimationFrame(playing); //this could get a bit laggy? may have to change
   }
 };
 
@@ -197,16 +168,29 @@ let warpBtn = () => {
     position.value * zoom.value.x * 150;
 };
 
-let tempo = 120;
+let tempo = 120; // beats per minute
 let timeSig = {
-  top: 4,
-  bottom: 4,
+  top: 4,  // how many beats in a measure
+  bottom: 4, // the length of a beat, eg 4 is quarter note (1/4) for one beat
 };
-let tempoInput = (e: Event) => {
-  e.preventDefault();
-  let val = Number((<HTMLDivElement>e.target!).innerText);
-  if (!isNaN(val)) tempo = val > 999 ? 999 : val < 10 ? 10 : val;
-  (<HTMLDivElement>e.target!).innerText = tempo.toFixed(0).toString();
+let inputs = (e: Event) => {  //div contenteditable inputs can't have v-model :)
+  let el = <HTMLDivElement>e.target!; // convert EventTarget to the Div element, also it must exist
+  let val: any = el.innerText;
+  if (el.id == "tempo") {
+    val = Number(val);
+    if (!isNaN(val)) tempo = val > 999 ? 999 : val < 10 ? 10 : val; // check if it is within bounds and a number
+    el.innerText = tempo.toFixed(0).toString(); // rounding
+  }
+  if (el.title == "time signature top") {
+    val = Number(val);
+    if (!isNaN(val)) timeSig.top = val > 40 ? 40 : val < 1 ? 1 : val;
+    el.innerText = timeSig.top.toFixed(0).toString();
+  }
+  if (el.title == "time signature bottom") {
+    val = Number(val);
+    if (!isNaN(val)) timeSig.bottom = Math.log2(val) % 1 == 0 ? val : timeSig.bottom;
+    el.innerText = timeSig.bottom.toFixed(0).toString();
+  }
 };
 
 let parts = ref(<Part[]>[
@@ -228,7 +212,11 @@ let parts = ref(<Part[]>[
         ],
       },
     ],
-    nodes: [],
+    nodes: [{
+      x: 100,
+      y: 100,
+      name: "hey",
+    }],
   },
   {
     name: "piano2",
@@ -248,9 +236,9 @@ let parts = ref(<Part[]>[
   },
 ]);
 
-let pageModify = ref(<Modify>{ pan: { x: 0, y: 0 }, cur: null });
+let pageModify = ref(<Modify>{ pan: { x: 50, y: 50, moving: false }, cur: null, master: [] });
 
-parts.value.forEach((p) => {
+parts.value.map((p) => {
   if (p.type == "midi") {
     let pitches = p.content.flatMap((c) =>
       (<MidiClip>c).notes.map((n) => n.pitch)
